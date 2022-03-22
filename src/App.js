05b4303130a6afdcfe8ca7a7fc68fd549ac6406a
romaulisianipar24 <romaulimauliate@gmail.com>
2022-03-22 21:43:16 +0700

    push baru

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import data from './component/load';
+import Button from './component/button';
+
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
+    <h1>Track Info</h1>
+    <div className='group'>
       <div>
-        <input />
-        <button>Search</button>
+        <img src={data.album.images[0].url} width="300" height="300" className="rotate" />
+      </div>
+      <div>
+        <h2>{data.name}</h2>
+        <h3>{data.artists[0].name} : {data.album.name}</h3>
+        <Button />
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
